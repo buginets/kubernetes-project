1c57a4b20746684b78244db547da855fe159af2f
buginets <adibaigaziev@gmail.com>
2025-06-11 20:06:00 +0000

    cmd

---
+++ project1/cmd.sh
@@ -0,0 +1,10 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx --create-namespace \
+  --set controller.publishService.enabled=true
+
+
+
+kubectl get svc -n ingress-nginx
